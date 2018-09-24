4d2b415eb7b34eccc04b43fb3a136cbcd3f163ff
oscar <oscar@1DAM-PROFESOR.info.quevedo.local>
2018-09-24 13:08:58 +0200

    mensaje nuevo

--- MiprimerJava/src/miprimerjava/MiprimerJava.java
+++ MiprimerJava/src/miprimerjava/MiprimerJava.java
@@ -17,9 +17,22 @@ public class MiprimerJava {
      */
     public static void main(String[] args) {
 
+        int x = 9;
+        double a = 5.7;
+        x = (int) a;
+        x++;
+        x = x + 1;
+
+        boolean siono = true;
+        boolean no = false;
+        boolean valorBool = x > 9;
+        char caracter = 'a';
+        caracter = '\n';
 // ahora voy a poner una linea que hace el hola mundo
 // T code application logic here
 // despoues de hacer pool
+System.out.println("hola");
+        System.out.println(x);  
     }
 
 }
