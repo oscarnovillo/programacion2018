66fc1b03ea4df88a18c8aff1693979c8257c57fe
oscar <oscar@1DAM-PROFESOR.info.quevedo.local>
2018-10-01 13:20:00 +0200

    Merge origin/master
    Conflicts:
    	JavaApplication3/src/javaapplication3/JavaApplication3.java

---
+++ MiprimerJava/src/miprimerjava/TablaDel5.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miprimerjava;
+
+/**
+ *
+ * @author oscar
+ */
+public class TablaDel5 {
+  
+  public static void main(String[] args) {
+    
+    
+    int numero = 0;
+    
+    while (numero  <= 10)
+    {
+      System.out.println(" 5 X "+numero 
+	      +" = "+(numero*5));
+      numero ++;
+    }
+    
+  }
+  
+}
