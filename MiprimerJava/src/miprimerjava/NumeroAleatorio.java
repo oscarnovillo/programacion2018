9763a0be17e9c21a164f9ad8879d6f957e4e0050
oscar <oscar@1DAM-PROFESOR.info.quevedo.local>
2018-10-04 09:20:04 +0200

    Merge Nuevo
    Conflicts:
    	JavaApplication3/src/javaapplication3/JavaApplication3.java

---
+++ MiprimerJava/src/miprimerjava/NumeroAleatorio.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miprimerjava;
+
+import java.util.Random;
+
+/**
+ *
+ * @author oscar
+ */
+public class NumeroAleatorio {
+
+  public static void main(String[] args) {
+
+    Random r = new Random();
+
+    System.out.println(r.nextInt(21) -10);
+    System.out.println(r.nextInt(10) + 1);
+    System.out.println(r.nextInt(10) + 1);
+    System.out.println(r.nextInt(10) + 1);
+    System.out.println(r.nextInt(10) + 1);
+    System.out.println(r.nextInt(10) + 1);
+    System.out.println(r.nextInt(10) + 1);
+    System.out.println(r.nextInt(10) + 1);
+    System.out.println(r.nextInt(10) + 1);
+    System.out.println(r.nextInt(10) + 1);
+    System.out.println(r.nextInt(10) + 1);
+    System.out.println(r.nextInt(10) + 1);
+    System.out.println(r.nextInt(10) + 1);
+
+  }
+
+}
