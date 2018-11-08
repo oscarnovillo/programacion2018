3d71cf97abe1a75669340f32c2cf08b4589e82ff
oscar <oscar@1DAM-PROFESOR.info.quevedo.local>
2018-11-08 09:44:22 +0100

    jgkiyhku

--- testProject/src/test/java/test/FirstJUnit5Tests.java
+++ testProject/src/test/java/test/FirstJUnit5Tests.java
@@ -1,14 +1,23 @@
 package test;
 
+
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.DisplayName;
 
 import org.junit.jupiter.api.Test;
 
+@DisplayName("A special test case")
 public class FirstJUnit5Tests {
 
     @Test
+    @DisplayName("mi primera prueba")
     public void myFirstTest() {
         assertEquals(2, 1 + 1);
+	assertAll("nombre" ,
+	() -> assertTrue(true));
+	
     }
 
 }
