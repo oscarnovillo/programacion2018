150c5cb0071a37858b062ebf61011cb31145de92
oscar <oscar@DESKTOP-E8BOLD9>
2018-12-09 21:11:07 +0100

    test

--- testMaven/src/test/java/test/TestArrays.java
+++ testMaven/src/test/java/test/TestArrays.java
@@ -5,7 +5,10 @@
  */
 package test;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.InputStream;
@@ -17,6 +20,8 @@ import java.util.Scanner;
 import main.JavaArrays;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 /**
  *
@@ -24,16 +29,25 @@ import org.junit.jupiter.api.Test;
  */
 @DisplayName("me llamo juan")
 public class TestArrays {
-    
-    @Test
-    void testEjercicio1() throws FileNotFoundException
-    {
-String out = "";
-        ByteArrayInputStream input = new ByteArrayInputStream("1\n2\n3\n4\n5\n".getBytes());
-        PrintStream w = new PrintWriter( out);
-        System.setOut(w);
-        Scanner sc =  new Scanner(input);
-        JavaArrays.Ejercicio1(sc);
-    }
-    
+
+
+	@ParameterizedTest 
+	@ValueSource(strings = {"1\r\n2\r\n3\r\n4\r\n5\r\n","2\n3\n4\n4\n5\n"})
+	void testEjercicio1(String in)  {
+
+		ByteArrayInputStream input = new ByteArrayInputStream(in.getBytes());
+		final ByteArrayOutputStream myOut = new ByteArrayOutputStream();
+		PrintStream outOld = System.out;
+		System.setOut(new PrintStream(myOut));
+		Scanner sc = new Scanner(input);
+		JavaArrays.Ejercicio1(sc);
+		System.setOut(outOld);
+		String sOut = myOut.toString();
+
+		System.out.println(myOut.toString());
+
+		assertEquals(in, sOut.substring(sOut.indexOf("5") + 3));
+
+	}
+
 }
