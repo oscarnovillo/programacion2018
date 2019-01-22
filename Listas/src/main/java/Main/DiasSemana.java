632cb713cc535f09de35791af3b431c38e839875
oscar <oscar@DESKTOP-E8BOLD9>
2019-01-22 19:52:06 +0100

    Merge origin/master
    Conflicts:
    	PrimerJavaFX/pom.xml
    	PrimerJavaFX/src/main/java/model/Item.java

---
+++ Listas/src/main/java/Main/DiasSemana.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+/**
+ *
+ * @author oscar
+ */
+public enum DiasSemana {
+    
+    LXM, MJ;
+    
+    public static String volver(DiasSemana dia)
+            
+    {
+        return "hola";
+    }
+}
