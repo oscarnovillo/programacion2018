08cbdc3795a200e8d2f36ba32e0bab46029e9d8f
oscar <oscar@1DAM-PROFESOR.info.quevedo.local>
2019-01-23 10:12:11 +0100

    Merge origin/master
    Conflicts:
    	buscaminasObjetos/src/servicios/Buscaminas.java

--- Listas/src/main/java/Main/Main.java
+++ Listas/src/main/java/Main/Main.java
@@ -8,6 +8,7 @@ package Main;
 import daw.listas.Alumno;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -16,19 +17,61 @@ import java.util.stream.Collectors;
  * @author oscar
  */
 public class Main {
-    
-    
-    public static void main(String[] args) {
-        
-        List<Alumno> l = new ArrayList();
-        l.add(new Alumno("hjjj"));
-        l.add(new Alumno("aaaj"));
-        l.add(new Alumno("hvvvj"));
-        
-       List<Alumno> filtro =  l.stream().filter(t -> t.nombre.contains("f")).collect(Collectors.toList());
-      
-       filtro.get(0).nombre = "jjj";
-       
-       System.out.println("hola");
+
+  public static void main(String[] args) {
+
+    ArrayList<Integer> enteros = new ArrayList();
+
+    enteros.add(8);
+
+    ArrayList<Alumno> listaAlumnos = new ArrayList();
+
+    Alumno a1 = new Alumno("1");
+    Alumno a2 = new Alumno("2");
+    Alumno a3 = new Alumno("3");
+    listaAlumnos.add(new Alumno("1"));
+    listaAlumnos.add(a2);
+    listaAlumnos.add(a3);
+
+    //tamaño de la lista
+    System.out.println(listaAlumnos.size());
+
+    Alumno temp = listaAlumnos.get(0);
+    System.out.println(temp);
+    temp = listaAlumnos.get(2);
+    System.out.println(temp);
+
+    listaAlumnos.remove(1);
+    System.out.println(listaAlumnos.size());
+
+    for (int i = 0; i < listaAlumnos.size(); i++) {
+      System.out.println(listaAlumnos.get(i));
     }
+
+    System.out.println(listaAlumnos.contains(a2));
+    System.out.println(listaAlumnos.indexOf(a3));
+    listaAlumnos.remove(a3);
+    System.out.println(listaAlumnos.indexOf(a3));
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println("dame getNombre() alumno a buscar");
+    String nombre = sc.nextLine();
+
+    Alumno buscar = new Alumno(nombre);
+    
+     System.out.println("indice de manera lista");
+    System.out.println(listaAlumnos.indexOf(buscar));
+    buscar.setDireccion("nueva");
+    
+    int index = listaAlumnos.indexOf(buscar);
+    listaAlumnos.get(index).setDireccion("nueva ");
+    
+    System.out.println("indice de manera array normal");
+    for (int i = 0; i < listaAlumnos.size(); i++) {
+      if (listaAlumnos.get(i).getNombre().equals(nombre)) {
+	System.out.println(i);
+      }
+    }
+
+  }
 }
--- Listas/src/main/java/daw/listas/Alumno.java
+++ Listas/src/main/java/daw/listas/Alumno.java
@@ -5,18 +5,71 @@
  */
 package daw.listas;
 
+import java.util.Objects;
+
 /**
  *
  * @author oscar
  */
 public class Alumno{
     
-    public String nombre;
+    private String nombre;
+    private String direccion;
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public String getDireccion() {
+    return direccion;
+  }
+
+  public void setDireccion(String direccion) {
+    this.direccion = direccion;
+  }
+
+    
+    
+  @Override
+  public int hashCode() {
+    int hash = 7;
+    hash = 61 * hash + Objects.hashCode(this.nombre);
+    return hash;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    final Alumno other = (Alumno) obj;
+    if (!Objects.equals(this.nombre, other.nombre)) {
+      return false;
+    }
+    return true;
+  }
+    
 
     public Alumno(String nombre) {
         this.nombre = nombre;
     }
 
+  @Override
+  public String toString() {
+    return "Alumno{" + "nombre=" + nombre + '}';
+  }
+
+  
 
     
     
