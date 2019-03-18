512320d80b1be668ce9173b42a031859f62af9be
oscar <oscar@1DAM-PROFESOR.info.quevedo.local>
2019-03-18 13:13:21 +0100

    Merge origin/master
    Conflicts:
    	buscaminasObjetos/src/servicios/Buscaminas.java

---
+++ CarcasaJavaFX/src/main/java/fx/controllers/FXMLAbrirSobreController.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fx.controllers;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+
+/**
+ * FXML Controller class
+ *
+ * @author oscar
+ */
+public class FXMLAbrirSobreController implements Initializable {
+
+  @FXML
+  private ComboBox<String> fxComboNiñato;
+  @FXML
+  private ComboBox fxComboAlbum;
+  
+  
+  
+   private FXMLBorderPaneController principalController;
+
+  public void setPrincipalController(FXMLBorderPaneController principalController) {
+    this.principalController = principalController;
+  }
+  
+  public void cargarNiñatos(ArrayList<String> niñatos)
+  {
+    fxComboNiñato.getItems().clear();
+    fxComboNiñato.getItems().addAll(niñatos);
+  }
+  
+  
+  @FXML
+  public void cambiaNiñato(ActionEvent event)
+  {
+    String seleccion = 
+	    fxComboNiñato.getSelectionModel()
+		    .getSelectedItem();
+    
+    ArrayList<String> albums = principalController.dameAlbumsNiñato(seleccion);
+    fxComboAlbum.getItems().clear();
+    fxComboAlbum.getItems().addAll(albums);
+  }
+  
+  /**
+   * Initializes the controller class.
+   */
+  @Override
+  public void initialize(URL url, ResourceBundle rb) {
+    // TODO
+    
+    
+  }  
+  
+}
--- CarcasaJavaFX/src/main/java/fx/controllers/FXMLBorderPaneController.java
+++ CarcasaJavaFX/src/main/java/fx/controllers/FXMLBorderPaneController.java
@@ -7,6 +7,7 @@ package fx.controllers;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -39,15 +40,15 @@ public class FXMLBorderPaneController implements Initializable {
   public void setNombre(String nombre) {
     this.nombre = nombre;
   }
-  
-  
-  
-  
-  private AnchorPane pagPrincipal;
-  private AnchorPane pag2;
-  private FXMLPrimeraPantallaController principalController;
-  private FXMLPagina2Controller pag2Controller; 
 
+  private AnchorPane pagPrincipal;
+  private FXMLPrimeraPantallaController principalController;
+
+  private AnchorPane pag2;
+  private FXMLPagina2Controller pag2Controller;
+
+  private AnchorPane abrirSobre;
+  private FXMLAbrirSobreController abrirSobreController;
 
   public FXMLPagina2Controller getPag2Controller() {
     return pag2Controller;
@@ -57,6 +58,46 @@ public class FXMLBorderPaneController implements Initializable {
   public void cargar1(ActionEvent event) {
 
     cargarPrincipal();
+  }
+
+  public void cargarAbrirSobre() {
+    try {
+      if (abrirSobre == null) {
+	FXMLLoader loaderMenu
+		= new FXMLLoader(getClass().getResource("/fxml/FXMLAbrirSobre.fxml"));
+	abrirSobre = loaderMenu.load();
+
+	abrirSobreController = loaderMenu.getController();
+	abrirSobreController.setPrincipalController(this);
+      }
+
+      ArrayList<String> niños = new ArrayList<>();
+      niños.add("j");
+      niños.add("p");
+      niños.add("k");
+      abrirSobreController.cargarNiñatos(niños);
+
+      fxRoot.setCenter(abrirSobre);
+
+    } catch (Exception ex) {
+      Logger.getLogger(FXMLBorderPaneController.class.getName()).log(Level.SEVERE, null, ex);
+    }
+  }
+
+  public ArrayList<String> dameAlbumsNiñato(String niño) {
+    ArrayList<String> albums = new ArrayList<>();
+    switch (niño) {
+      case "j":
+	albums.add("1");
+	break;
+      case "p":
+	albums.add("3");
+	break;
+      default:
+	albums.add("2");
+    }
+
+    return albums;
   }
 
   public void cargarPrincipal() {
@@ -76,8 +117,7 @@ public class FXMLBorderPaneController implements Initializable {
     }
   }
 
-  
-   private void preCargarPag2() {
+  private void preCargarPag2() {
     try {
       FXMLLoader loaderMenu
 	      = new FXMLLoader(getClass().getResource("/fxml/FXMLPagina2.fxml"));
@@ -88,10 +128,9 @@ public class FXMLBorderPaneController implements Initializable {
       Logger.getLogger(FXMLBorderPaneController.class.getName()).log(Level.SEVERE, null, ex);
     }
   }
-	
-	
+
   public void cargarPag2() {
-      fxRoot.setCenter(pag2);
+    fxRoot.setCenter(pag2);
   }
 
   @FXML
