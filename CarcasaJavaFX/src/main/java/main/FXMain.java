d3d43a8435638bc372a019f1dc675f3ad19a6ca1
oscar <oscar@DESKTOP-E8BOLD9>
2019-06-22 21:48:58 +0200

    Merge origin/master
    Conflicts:
    	CarcasaJavaFX/src/test/java/test/NewEmptyJUnitTest.java

--- CarcasaJavaFX/src/main/java/main/FXMain.java
+++ CarcasaJavaFX/src/main/java/main/FXMain.java
@@ -7,14 +7,9 @@ package main;
 
 import java.io.IOException;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
